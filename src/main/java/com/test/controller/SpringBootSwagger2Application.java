1fb5bad5470eb9e79d89c212572ff2a984873890
harinathatechnical <47938954+harinathatechnical@users.noreply.github.com>
2019-09-07 16:06:00 +0530

    Update SpringBootSwagger2Application.java

--- src/main/java/com/test/controller/SpringBootSwagger2Application.java
+++ src/main/java/com/test/controller/SpringBootSwagger2Application.java
@@ -1,6 +1,6 @@
 package com.test.controller;
 
-import com.sun.deploy.ui.AppInfo;
+//import com.sun.deploy.ui.AppInfo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
